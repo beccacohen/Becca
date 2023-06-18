616b5580a4102d08b5baac9634ab0a1685a36c56
beccacohen <rcohen2@ufl.edu>
2023-06-17 21:11:22 -0400

    login created

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+// declare accessToken variable outside the function
+var accessToken; 
+
+// global account state
+var cur_account = { isLoggedIn: false, username: '', id: ''};
+
 // access token
-async function makeApiCall() {
+async function getSongs() {
   accessToken = await refreshAccessToken();
   // Return the fetch promise here
   return fetch('https://api.spotify.com/v1/playlists/4GtQVhGjAwcHFz82UKy3Ca', {
     headers: {
       'Authorization': 'Bearer ' + accessToken
     }
+  })
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    return data;
   });
 }
 
@@ -18,10 +30,52 @@ async function refreshAccessToken() {
       'Content-Type': 'application/x-www-form-urlencoded'
     }
   })
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      return data.access_token;
-    })
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    return data.access_token;
+  })
+}
+
+async function getAccounts() {
+  return fetch(
+    'http://demo8229037.mockable.io/swiftsorter/accounts'
+  )
+  .then(function (response) {
+    return response.json();
+  });
+}
+
+async function login() {
+  var accounts = await getAccounts()
+  // Retrieve the username and password input values
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  console.log(accounts);
+
+  accounts["login"].forEach(function(account) {
+    if (account.username === username && account.password === password) {
+      // valid account
+      cur_account.isLoggedIn = true
+      cur_account.username = account.username
+      cur_account.id = account.id
+
+      // Create element to display username
+      var usernameElement = document.createElement('h2');
+      usernameElement.textContent = 'Welcome ' + cur_account.username + '!';
+      document.body.appendChild(usernameElement);
+
+      // erase error message when user logs in successfully
+      var errorMessage = document.getElementById('error-message');
+      errorMessage.textContent = '';
+    }
+  });
+
+
+  if (cur_account.isLoggedIn === false) {
+    var errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = 'Invalid username or password';
+  }
 }
