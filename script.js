db959bca8148153dbf047e15f54e8b2906f0a005
beccacohen <rcohen2@ufl.edu>
2023-08-08 09:33:08 -0400

    Added create account function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // declare accessToken variable outside the function
-var accessToken; 
+var accessToken;
 
 // global account state
-var cur_account = { isLoggedIn: false, username: '', id: ''};
+var cur_account = { isLoggedIn: false, username: '', id: '' };
 
 // global song comparisons
 var comparisonArr = [];
@@ -16,11 +16,9 @@ function createComparisonArr(songArr) {
   for (var i = 0; i < songArr.length; i++) {
     // create all 2-song comparisons
     for (var j = i + 1; j < songArr.length; j++) {
-      comparisonArr.push({ song1: songArr[i], song2: songArr[j]});
+      comparisonArr.push({ song1: songArr[i], song2: songArr[j] });
     }
   }
-
-  console.log(comparisonArr);
 }
 
 function createLeaderboard(songArr) {
@@ -29,33 +27,32 @@ function createLeaderboard(songArr) {
   for (var i = 0; i < songArr.length; i++) {
     leaderboard[songArr[i]] = 0;
   }
-  console.log(leaderboard);
 }
 
 // access token
 async function getSongs() {
   accessToken = await refreshAccessToken();
   // Return the fetch promise here
-  return fetch('https://api.spotify.com/v1/playlists/4ZfPHff7Bf2ZEvvxJ5ZDWe', {
+  return fetch('https://api.spotify.com/v1/playlists/54RU4wkkgz7NZBA4AN0ukg', {
     headers: {
       'Authorization': 'Bearer ' + accessToken
     }
   })
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (data) {
-    return data;
-  });
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      return data;
+    });
 }
 
 async function createSongArr() {
   var songs = await getSongs();
   var songArr = [];
 
-  // replace the 100 with the length of the playlist
+  // replace the 23 with the length of the playlist
   // songs.tracks.items.length
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < 23; i++) {
     songArr.push(songs.tracks.items[i].track.name);
   }
 
@@ -72,14 +69,13 @@ async function refreshAccessToken() {
       'Content-Type': 'application/x-www-form-urlencoded'
     }
   })
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (data) {
-    return data.access_token;
-  })
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      return data.access_token;
+    })
 }
-
 
 function displayFinalRankings() {
   var rankingsList = document.getElementById('rankingsList');
@@ -94,12 +90,6 @@ function displayFinalRankings() {
     var listItem = document.createElement('li');
     listItem.textContent = (index + 1) + '. ' + song;
     rankingsList.appendChild(listItem);
-  });
-
-  // also save sorted leaderboard to database
-  fetch('https://demo8229037.mockable.io/swiftsorter/leaderboard', {
-    method: 'POST',
-    body: JSON.stringify(sortedLeaderboard)
   });
 }
 
@@ -147,7 +137,6 @@ function handleSongClick(selectedSong) {
     displayFinalRankings();
     var saveButton = document.getElementById('save-button');
     saveButton.style.display = '';
-    saveLeaderboard();
     return;
   }
 
@@ -170,15 +159,13 @@ async function initialize() {
 
 }
 
-
-
 async function getAccounts() {
   return fetch(
-    'http://demo8229037.mockable.io/swiftsorter/accounts'
+    'https://demo8229037.mockable.io/swiftsorter/accounts'
   )
-  .then(function (response) {
-    return response.json();
-  });
+    .then(function (response) {
+      return response.json();
+    });
 }
 
 async function logout() {
@@ -208,9 +195,7 @@ async function login() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
 
-  console.log(accounts);
-
-  accounts["login"].forEach(function(account) {
+  accounts["login"].forEach(function (account) {
     if (account.username === username && account.password === password) {
       // wipe input fields
       document.getElementById('username').value = '';
@@ -225,14 +210,14 @@ async function login() {
       var welcomeMessage = document.getElementById('welcome-message');
       welcomeMessage.textContent = 'Welcome ' + cur_account.username + '!';
 
-      // hide login form
+      // hide login form    
       var loginForm = document.getElementsByClassName('login-container')[0];
       for (var i = 0; i < loginForm.children.length; i++) {
         loginForm.children[i].style.display = 'none';
       }
 
       // erase error message when user logs in successfully
-      var errorMessage = document.getElementById('error-message');
+      var errorMessage = document.getElementById('login-error-message');
       errorMessage.textContent = '';
 
       // show logout button
@@ -240,14 +225,95 @@ async function login() {
       logoutButton.style.display = '';
     }
   });
-  
+
 
   if (cur_account.isLoggedIn === false) {
-    var errorMessage = document.getElementById('error-message');
+    var errorMessage = document.getElementById('login-error-message');
     errorMessage.textContent = 'Invalid username or password';
   }
 }
 
+async function signup() {
+  // hide login form
+  var loginForm = document.getElementsByClassName('login-container')[0];
+  loginForm.style.display = 'none';
+
+  // clear login form fields
+  document.getElementById('username').value = '';
+  document.getElementById('password').value = '';
+  document.getElementById('login-error-message').textContent = '';
+
+  // show signup form
+  var signupForm = document.getElementsByClassName('signup-container')[0];
+  signupForm.style.display = 'block';
+}
+
+async function back() {
+  // hide signup form
+  var signupForm = document.getElementsByClassName('signup-container')[0];
+  signupForm.style.display = 'none';
+
+  // clear signup form fields
+  document.getElementById('new-username').value = '';
+  document.getElementById('new-password').value = '';
+  document.getElementById('confirm-password').value = '';
+  document.getElementById('signup-error-message').textContent = '';
+
+  // show login form
+  var loginForm = document.getElementsByClassName('login-container')[0];
+  loginForm.style.display = 'block';
+}
+
+async function createAccount() {
+  // clear error message
+  var errorMessage = document.getElementById('signup-error-message');
+  errorMessage.textContent = '';
+
+  // get username and password from fields
+  var username = document.getElementById('new-username').value;
+  var password = document.getElementById('new-password').value;
+  var confirmPassword = document.getElementById('confirm-password').value;
+
+  // password validation
+  if (password !== confirmPassword) {
+    var errorMessage = document.getElementById('signup-error-message');
+    errorMessage.textContent = 'Passwords do not match';
+    return;
+  }
+
+  // username and password length validation
+  if (username.length < 4 || password.length < 4) {
+    var errorMessage = document.getElementById('signup-error-message');
+    errorMessage.textContent = 'Must be at least 4 characters';
+    return;
+  }
+
+  // check if username already exists
+  var accounts = await getAccounts();
+  accounts["login"].forEach(function (account) {
+    if (account.username === username) {
+      var errorMessage = document.getElementById('signup-error-message');
+      errorMessage.textContent = 'Username already exists';
+      return;
+    }
+  });
+
+  // create new account
+  var newAccount = {
+    "id": accounts["login"].length + 1,
+    "username": username,
+    "password": password
+  };
+
+  // post new account to server
+  fetch('https://demo8229037.mockable.io/swiftsorter/createaccount', {
+    method: 'POST',
+    body: JSON.stringify(newAccount),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
 
 // add event listener to password input
 var passwordInput = document.getElementById('password');
